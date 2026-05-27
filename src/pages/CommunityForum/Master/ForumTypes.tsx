98baa2f4c0828c38c5d44164d27766d11b330a1b
Chiran Jeewantha <chiranjonline@gmail.com>
2026-05-27 07:41:20 +0530

    Add pagination navigation handlers to ForumTypes component

--- src/pages/CommunityForum/Master/ForumTypes.tsx
+++ src/pages/CommunityForum/Master/ForumTypes.tsx
@@ -39,6 +39,8 @@ const ForumTypes: React.FC = () => {
     pagination,
     setCurrentPage,
     setItemsPerPage,
+    handlePrevPage,
+    handleNextPage,
     setPagination,
   } = usePagination();
 
